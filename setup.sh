b4452170c56a31ea85d15f557a60c5155ff9ba45
jsolbrig <jeremy.solbrig@colostate.edu>
2025-07-31 02:49:56 +0000

    Add cartopy data

--- setup.sh
+++ setup.sh
@@ -128,6 +128,7 @@ chmod +x /opt/start_jupyterlab.sh
     # Install data from s3 bucket
     aws s3 cp s3://geoips-tutorial/geoips_outdirs /tmp/geoips_outdirs --recursive
     aws s3 cp s3://geoips-tutorial/geoips_testdata_dir /tmp/geoips_testdata_dir --recursive
+    aws s3 cp s3://geoips-tutorial/cartopy /tmp/cartopy --recursive
 )
 
 # This function copies the conda environment to each user's home directory
@@ -158,8 +159,10 @@ setup_user_env() {
     # Copy data to user's home directory
     cp -r /tmp/geoips_outdirs/ "${user_home}/geoips_outdirs"
     cp -r /tmp/geoips_testdata_dir/ "${user_home}/geoips_test_data"
+    cp -r /tmp/cartopy/ "${user_home}/cartopy"
     chown -R "${user}:${user}" "${user_home}/geoips_outdirs"
     chown -R "${user}:${user}" "${user_home}/geoips_test_data"
+    chown -R "${user}:${user}" "${user_home}/cartopy"
 }
 
 # Export to make available in subshells
